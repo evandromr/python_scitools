faaf305f0ca536bbd018b983e26f4000ed72feb8
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2013-09-04 17:12:36 -0300

    added script to run the z2n periodogram making use of parallelization with parallel python

---
+++ z2nperiodogram.py
@@ -0,0 +1,53 @@
+#!/usr/env python
+
+import pp  # parallel python library
+import astropy.io.fits as fits  # FITS manipulating py library
+import numpy as np  # numeric python for array manipulation
+import os  # os utilitites
+import rayleighz2n
+
+
+if __name__ == '__main__':
+    #input file informations and variables atributions----------------------
+    inptname = str(raw_input('Input file (with extension): '))
+    outptname = os.path.splitext(inptname)[0]+'_z2n_output.fits'
+    inpt = fits.open(inptname)
+    time = inpt[1].data.field('TIME')
+    inpt.close()
+
+    startf = float(raw_input('Enter the start frequency: '))
+    endf = float(raw_input('Enter the last frequency: '))
+    deltaf = float(raw_input('Enter the frequency interval: '))
+    freqs = np.arange(startf, endf, deltaf)
+
+    harm = int(raw_input('The Harmonic to be considered:'))
+
+    #----------------- The parallelism starts here ------------------------
+    ncpus = int(raw_input('Enter the number of processor to use: '))
+    if ncpus < 1 :
+        ncpus = 1  # default number of cpus = 1
+
+    freqlist = np.array_split(freqs, ncpus)
+
+    ppservers = ()
+    job_server = pp.Server(ncpus, ppservers=ppservers)
+
+    print "\n Calculating with", job_server.get_ncpus(), "processor(s)\n"
+
+    jobs = [job_server.submit(rayleighz2n.rayleighz2n, (somefreqs, time, harm),
+        (), ("numpy",)) for somefreqs in freqlist]
+
+    results = []
+    [[results.append(value) for value in job()] for job in jobs]
+
+    # create and write the output.fits file
+    col1 = fits.Column(name='Frequency', format='E', unit='Hz', array=freqs)
+    col2 = fits.Column(name='Z2n', format='E', unit='arbitrary', array=results)
+    cols = fits.ColDefs([col1, col2])
+    tbhdu = fits.new_table(cols)
+    tbhdu.writeto(outptname)
+    print ' DONE!'
+    print ' Please, see the file {0} to results\n\n'.format(outptname)
+
+    #prints jobs information
+    job_server.print_stats()
