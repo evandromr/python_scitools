a293ea2ac7644914b2fb8a3ebe10d1664d8ab0bf
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2013-09-08 10:33:04 -0300

    add scattercolumns.py: same as plotcolumns, but scatter

---
+++ scattercolumns.py
@@ -0,0 +1,22 @@
+#!/usr/env python
+
+import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    '''
+    scatter 2 columns of a fits files
+    '''
+
+    inpt1 = str(raw_input("Nome do primeiro arquivo: "))
+    field1 = str(raw_input("Nome da coluna X: "))
+    field2 = str(raw_input("Nome da coluna Y: "))
+
+    tb1 = fits.open(inpt1)
+
+    x = tb1[1].data.field(field1)
+    y = tb1[1].data.field(field2)
+
+    plt.scatter(x, y)
+    plt.show()
