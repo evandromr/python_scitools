355177dc7002a0c0c4488002ebd71159c298906b
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-02-18 14:34:29 -0300

    fix typo in lcperiod.py

--- lcperiod.py
+++ lcperiod.py
@@ -11,17 +11,17 @@ lc = fits.open(inpt)
 # Convert to big-endian array is necessary to the lombscargle function
 time = np.array(lc[1].data["TIME"], dtype='float64')
 rate = np.array(lc[1].data["RATE"], dtype='float64')
-norm = timecolumn.shape[0]
+norm = time.shape[0]
 
 # Probably buggy, check the units (angular vs Hz)
-freqs = np.linspace(0.0001,0.001,900)
+freqs = np.linspace(0.0001, 0.001, 900)
 
 # Ther periodogram itself
-pgram = ss.lombscargle(time,rate,freqs)
+pgram = ss.lombscargle(time, rate, freqs)
 
 # Plot lightcurve on top panel and powerspectrum on bottom panel
-plt.subplot(2,1,1)
-plt.plot(time,rate,'b+')
-plt.subplot(2,1,2)
-plt.plot(freqs,np.sqrt(4*(pgram/norm)))
+plt.subplot(2, 1, 1)
+plt.plot(time, rate, 'k-')
+plt.subplot(2, 1, 2)
+plt.plot(freqs, np.sqrt(4*(pgram/norm)), 'b-')
 plt.show()
