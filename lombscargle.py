dc9023c78a522355376117416b40decde38e0211
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-02-26 12:37:52 -0300

    lombscargle script with normalized count rate

--- lombscargle.py
+++ lombscargle.py
@@ -5,6 +5,7 @@ import scipy.signal as ss
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 
+
 inpt = str(raw_input("Nome do Arquivo: "))
 lc = fits.open(inpt)
 bin = float(raw_input("bin size (or camera resolution): "))
@@ -12,9 +13,13 @@ oversample = float(raw_input('Fator de Oversample:'))
 
 # Convert to big-endian array is necessary to the lombscargle function
 time = np.array(lc[1].data["TIME"], dtype='float64')
+time -= time.min()
 rate = np.array(lc[1].data["RATE"], dtype='float64')
-norm = time.shape[0]
 
+# Exclude NaN and negative values -------------------------
+print ''
+print 'Excluding nan and negative values...'
+print ''
 exclude = []
 for i in xrange(len(rate)):
     if rate[i] > 0:
@@ -25,32 +30,44 @@ for i in xrange(len(rate)):
 exclude = np.array(exclude)
 nrate = np.delete(rate, exclude)
 ntime = np.delete(time, exclude)
+#-----------------------------------------------------------
 
-print 'rate array'
-print rate
-print ''
-print 'Excluding nan and negative values...'
-print ''
+# normalize rate array
+nrate -= nrate.mean()
+nrate /= nrate.std()
 
-timemax = max(time)
-timemin = min(time)
-interval = timemax-timemin
+# normalization to the periodogram
+norm = ntime.shape[0]
+
+# duration of observation
+interval = time.max()-time.min()
+
+# minimum frequency limited by observed time
 freqmin = 2.0/interval
+
+# maximium frequency limited by time resolution
 freqmax = 1.0/bin
+
+# size of the array of frequencies
 nint = oversample/freqmin
-# Probably buggy, check the units (angular vs Hz)
+
+# Frequency array
 freqs = np.linspace(freqmin, freqmax, nint)
+
+# scipy.signal.lombscargle uses angular frequencies
 afreqs = 2.0*np.pi*freqs
 
 # Ther periodogram itself
 pgram = ss.lombscargle(ntime, nrate, afreqs)
 
-# Plot lightcurve on top panel and powerspectrum on bottom panel
+# Plot lightcurve on top panel
 plt.subplot(2, 1, 1)
 plt.plot(time, rate, 'bo-')
 plt.xlabel('Time [s]', fontsize=12)
-plt.ylabel('Rate [counts/s]', fontsize=12)
-plt.xlim(timemin, timemax)
+plt.ylabel('Normalized Count Rate [counts/s]', fontsize=12)
+plt.xlim(time.min(), time.max())
+
+# Plot powerspectrum on bottom panel
 plt.subplot(2, 1, 2)
 plt.plot(freqs, np.sqrt(4*(pgram/norm)), 'b-')
 plt.xlabel('Frequency [Hz]', fontsize=12)
