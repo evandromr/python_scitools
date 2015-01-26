12cfa1226665abca7d4f12de492b23e23ae57627
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2015-01-26 15:02:48 -0300

    legendas em portugues

--- plothard.py
+++ plothard.py
@@ -28,23 +28,10 @@ if __name__ == "__main__":
     src.close()
     bkg.close()
 
-    #x -= min(x)
-
-    # def contfunc(x, a):
-    #     return a
-    # def linfunc(x, a, b):
-    #     return (a*x) + b
-    # def sqrfunc(x, a, b, c):
-    #     return a*(x**2) + b*x + c
-
     r = yb/y
     s = yb+y
 
     slope, mean, rval, pval, stderr =  ss.linregress(r,s)
-
-    #popt, pcov = so.curve_fit(contfunc, r, s)
-    #popt, pcov = so.curve_fit(linfunc, r, s)
-    #perr = np.sqrt(np.diag(pcov))
 
     print 'slope =', slope
     print 'mean =', mean
@@ -54,12 +41,10 @@ if __name__ == "__main__":
     plt.errorbar(y+yb, yb/y, xerr=(e+eb), yerr=(((yb*e)-(y*eb))/(y**2)),
                  ls='none', fmt='bo')
     plt.plot(slope*r + mean, r, 'r', linewidth=2)
-    plt.xlabel('Rate$_1$ + Rate$_2$ (cts/s)')
-    plt.ylabel('Rate$_2$/Rate$_1$')
+    plt.xlabel('Cts. s$^{-1}$_{(1+2)}$')
+    plt.ylabel('Cts. s$^{-1}$_{(2)}$ / Cts. s$^{-1}$$_{(1)}$')
     plt.xlim(min(s), max(s))
-
-    plt.savefig(cam+'_hard_'+eng+'keV_'+eng2+'keV_bin'+binsize+'.pdf',
-                orientation='landscape', papertype='a4',
+    plt.savefig('plothard.pdf', orientation='landscape', papertype='a4',
                 format='pdf', bbox_inches='tight')
     plt.show()
     plt.clf()
